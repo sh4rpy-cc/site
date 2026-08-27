43c3f534f26e967ab92421031df1e73ee1a0c89e
Evdakim <loekoffcl@mail.ru>
2026-08-27 20:18:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
+import secrets
 
-SECRET_KEY = os.getenv("SECRET_KEY", "change-this-to-random-string")
+SECRET_KEY = os.getenv("SECRET_KEY") or secrets.token_hex(32)
 DB_PATH = "database.db"
-ADMIN_LOGIN = "admin"
-ADMIN_PASSWORD = "admin"
+ADMIN_LOGIN = "sh4rpy"
+ADMIN_PASSWORD = "cascascas1Z"
 API_PORT = 80
