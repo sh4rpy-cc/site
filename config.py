ca113700ee89df22c1924af07b79af7f98765a2a
Evdakim <loekoffcl@mail.ru>
2026-09-07 18:19:18 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-import secrets
-
-SECRET_KEY = os.getenv("SECRET_KEY") or secrets.token_hex(32)
-DB_PATH = "database.db"
-ADMIN_LOGIN = "sh4rpy"
-ADMIN_PASSWORD = "cascascas1Z"
-API_PORT = 80
